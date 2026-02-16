010e815de0bf502fbbf8a44e1d022f55b83283e9
TakaDev-Code <takaarakawa17@gmail.com>
2026-02-16 09:35:28 +0700

    Kedua

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/home/home";
 function App() {
     return (
         <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Registeration />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
